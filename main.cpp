b903a1b3aae3ad84e791af5cadfc2e5b9f734bab
Fritz Aseph Ruaro <qfaruaro@tip.edu.ph>
2024-09-09 11:54:01 +0800

    Update .cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	
-	double num1, num2, num3;
+	float num1, num2, num3;
 	cout<<"Enter three numbers:";
 	cin >>num1>>num2>>num3;
 	
-	double sum=num1 + num2 + num3;
+	float sum=num1 + num2 + num3;
 	cout<<"Addition:"<<sum<<endl;
 	
-	double difference= num1 - num2 - num3;
+	float difference= num1 - num2 - num3;
 	cout<<"Subtraction:"<<difference<<endl;
 	
-	double product= num1 * num2 * num3;
+	float product= num1 * num2 * num3;
 	cout<<"Multiplication:"<<product<<endl;
 	
-	double quotient= num1 / num2 / num3;
+	float quotient= num1 / num2 / num3;
 	cout<<"Division:"<<quotient<<endl;
 	
 	return 0;
