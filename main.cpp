04867f22cc6c62112f6ee31f6939b56080994934
Fritz Aseph Ruaro <qfaruaro@tip.edu.ph>
2024-09-09 11:21:39 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	float num1, num2, num3;
+	cout<<"Enter three numbers:";
+	cin >>num1>>num2>>num3;
+	double sum=num1 + num2 + num3;
+	cout<<"Addition:"<<sum<<endl;
+	
+	double difference= num1 - num2 - num3;
+	cout<<"Subtraction:"<<difference<<endl;
+	
+	double product= num1 * num2 * num3;
+	cout<<"Multiplication:"<<product<<endl;
+	
+	double quotient= num1 / num2 / num3;
+	cout<<"Division:"<<quotient<<endl;
+	
+	return 0;
+}
