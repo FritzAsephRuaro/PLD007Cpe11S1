5002711638247dec126e0d7d5777fb106b27f3f4
Fritz Aseph Ruaro <qfaruaro@tip.edu.ph>
2024-09-09 13:02:01 +0800

    Add files via upload

---
+++ Exercise2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	cout<<"	Algorithm to get the sum, difference, product, quotient, and remainder"<<endl;
+	
+	int x = 4, y = 2;
+	cout<<"	x + y = "<< x + y << endl;
+	cout<<"	x - y = "<< x - y <<endl;
+	cout<<"	x * y = "<< x * y <<endl;
+	cout<<"	x / y = "<< x / y <<endl;
+	cout<<"	x % y = "<< x % y <<endl;
+	
+	return 0;
+}
