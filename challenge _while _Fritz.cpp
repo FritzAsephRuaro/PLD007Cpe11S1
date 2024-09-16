2e1282f9391968310db501b7c88fe10e346b4ab7
Fritz Aseph Ruaro <qfaruaro@tip.edu.ph>
2024-09-16 12:20:49 +0800

    Add files via upload

---
+++ challenge _while _Fritz.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	int i = 3;
+	while(i<=9)
+	{
+	cout<<"Integers that is less than 10 but not less than 3 are "<<" Integers "<< i <<endl;
+	i=i+1;
+	}
+return 0;
+}
