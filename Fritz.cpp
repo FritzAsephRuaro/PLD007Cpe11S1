66b204acfa0ae71654dbd1c39b987c71334938ec
Fritz Aseph Ruaro <qfaruaro@tip.edu.ph>
2024-09-25 11:38:58 +0800

    Add files via upload

--- Fritz.cpp
+++ Fritz.cpp
@@ -3,24 +3,56 @@ using namespace std;
 
 int main()
 {
-int x;
-int y;
-//comment initiation of variables 
-	cout<<"Input two integers:"<<endl;
+//Case switch
+//create a c++ program to dertermine if a alphabet is vowel or consonant
 	
-		cout<<"Input the value of x";
-	cin>>x;
-		cout <<"Input three value of y";
-	cin>>y;
+	char c;
 
-	if (x<y){
-		cout<<"x is less than y"<<endl;
-}
-	else if(x>y){
-		cout<<"x is greater than y"<<endl;
-}
-	else{
-		cout<<"x is equal to y"<<endl;
+	cout<<"Enter the alphabet: ";
+	cin>>c;
+	
+	switch(c)
+	{
+		case'a':
+			cout<<"vowel";
+		break;	
+		
+		case'A':
+			cout<<"vowel";
+		break;
+		
+		case 'e':
+			cout<<"vowel";	
+		break;
+		
+		case 'E':
+			cout<<"vowel";
+		break;
+		
+		case 'i':
+			cout<<"vowel";
+		break;
+		
+		case 'o':
+			cout<<"vowel";
+		break;
+		
+		case 'O':
+			cout<<"vowel";
+		break;
+		
+		case 'u':
+			cout<<"vowel";
+		break;
+		
+		case 'U':
+			cout<<"vowel";
+		break;
+		
+		default:
+			cout<<"Consonant";
+		break;			
 	}
+	
 return 0;
 }
